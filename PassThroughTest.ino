d8639928887fdb2f2dfe45b4df5c3ad951b8d9b2
alanoatwork <alanandcorin@yahoo.com>
2017-06-29 15:54:52 -0700

    Add files via upload

---
+++ PassThroughTest.ino
@@ -0,0 +1,26 @@
+#include <DashSystemSerialGSM.h>
+DashSystemSerialGSM cell;
+
+void setup() {
+  Serial.begin();
+
+  HologramCloud.enterPassthrough();
+
+  cell.Boot();
+  cell.DeleteAllSMS();
+  cell.FwdSMS2Serial();
+
+  cell.Rcpt("+19495472010");
+  cell.Message("hello world");
+  cell.SendSMS();
+}
+
+void loop() {
+   if (cell.ReceiveSMS()){
+     Serial.print("Sender: ");
+     Serial.println(cell.Sender());
+     Serial.print("message: ");
+     Serial.println(cell.Message());
+     cell.DeleteAllSMS();
+  }
+}
